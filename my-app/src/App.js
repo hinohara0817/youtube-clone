b8ececab0cabc4ee350ebaa1e47ca33aa62b6677
hinohara <hinoshin0817@gmail.com>
2020-07-09 09:58:16 +0900

    初歩

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
+      <Counter></Counter>
+      <User name={"Taro"} />
+      <User name={"Shinya"} />
+      <User />
     </div>
   );
+}
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {count: 0}
+  }
+  handlePlusButton = () => {
+    const currentCount = this.state.count
+    this.setState({count: currentCount + 1})
+  }
+  render(){
+    return (
+      <React.Fragment>
+        <div>test {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+</button>
+      </React.Fragment>
+    )
+  }
+}
+const User = (props) => {
+  return <div>Hi, I am {props.name}</div>
+}
+
+User.defaultProps = {
+  name: "Hanako"
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  name: PropTypes.string.isRequired
 }
 
 export default App;
