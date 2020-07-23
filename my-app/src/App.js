f71bd50a2392627c7c30e89232acffbb7ded0230
hinohara <hinoshin0817@gmail.com>
2020-07-23 11:12:40 +0900

    youtube clone作成

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,52 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+import youtube from './api/youtube'; 
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-      <Counter></Counter>
-      <User name={"Taro"} />
-      <User name={"Shinya"} />
-      <User />
-    </div>
-  );
-}
-
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = {count: 0}
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
   }
-  handlePlusButton = () => {
-    const currentCount = this.state.count
-    this.setState({count: currentCount + 1})
+  componentDidMount(){
+    this.handleSubmit('test')
+  }
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video});
+  }
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyDmNop8n0RHCjZv0S8GaPZW9s5Sx5WA_ag',
+        q: searchTerm,
+      }
+    });
+    this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
   }
   render(){
-    return (
-      <React.Fragment>
-        <div>test {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+</button>
-      </React.Fragment>
+    const {selectedVideo, videos} = this.state;
+    return(
+      <Grid justify="center" container spacing={10} >
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     )
   }
 }
-const User = (props) => {
-  return <div>Hi, I am {props.name}</div>
-}
-
-User.defaultProps = {
-  name: "Hanako"
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  name: PropTypes.string.isRequired
-}
-
 export default App;
